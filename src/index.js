e6cde3a11f34b87a34a895e858de043ce947d226
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2023-04-02 14:54:48 +0300

    base redux + js ]

--- src/index.js
+++ src/index.js
@@ -3,16 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createStore } from "redux";
 
+const initialState = { value: 0 }
 
-
-const reducer = (state = 1, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INC':
-            return state + 1;
+            return {
+                ...state,
+                value: state.value + 1
+            };
         case 'DEC':
-            return state - 1;
+            return {
+                ...state,
+                value: state.value - 1
+            };
         case 'RND':
-            return state * action.payload;
+            return {
+                ...state,
+                value: state.value * action.payload
+            };
         default:
             return state;
     }
@@ -21,22 +30,25 @@ const reducer = (state = 1, action) => {
 const store = createStore(reducer);
 
 const update = () => {
-    document.getElementById('counter').textContent = store.getState();
+    document.getElementById('counter').textContent = store.getState().value;
 }
 
 store.subscribe(update);
 
+const inc = () => ({ type: 'INC' })
+const dec = () => ({ type: 'DEC' })
+const rnd = (value) => ({ type: 'RND', payload: value })
 
 document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch({ type: 'INC' });
+    store.dispatch(inc());
 });
 
 document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch({ type: 'DEC' });
+    store.dispatch(dec());
 });
 document.getElementById('rnd').addEventListener('click', () => {
     const value = Math.floor(Math.random() * 10);
-    store.dispatch({ type: 'RND', payload: value });
+    store.dispatch(rnd(value));
 });
 
 
